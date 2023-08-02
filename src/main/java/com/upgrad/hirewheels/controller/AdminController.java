5f546d8e0b77f87f897428ded18f4c3aa471cbea
Ayan Chakravorty <ayanchakravorty@gmail.com>
2023-08-02 23:05:10 +0530

    Checkpoint 3

--- src/main/java/com/upgrad/hirewheels/controller/AdminController.java
+++ src/main/java/com/upgrad/hirewheels/controller/AdminController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hirewheels/v1")
@@ -26,11 +23,20 @@ public class AdminController {
     @PostMapping(value = "/vehicles", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<VehicleDTO> addVehicle(@RequestBody VehicleDTO vehicleDTO) throws Exception {
         Vehicle newVehicle = modelMapper.map(vehicleDTO, Vehicle.class);
-        System.out.println("New Vehicle: "+ newVehicle);
         Vehicle savedvehicle = adminService.registerVehicle(newVehicle);
 
         VehicleDTO savedVehicleDTO = modelMapper.map(savedvehicle, VehicleDTO.class);
 
         return new ResponseEntity(savedVehicleDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/vehicles/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateVehicle(@PathVariable("id") int id, @RequestBody VehicleDTO vehicleDTO) {
+        Vehicle getVehicle = modelMapper.map(vehicleDTO, Vehicle.class);
+        Vehicle savedVehicle = adminService.changeAvailability(id, getVehicle.getAvailabilityStatus());
+
+        VehicleDTO savedVehicleDTO = modelMapper.map(savedVehicle, VehicleDTO.class);
+
+        return new ResponseEntity(savedVehicleDTO, HttpStatus.ACCEPTED);
     }
 }
